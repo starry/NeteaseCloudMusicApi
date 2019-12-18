2f22d3acb5cdb115e4eba29d8db03df987f739aa
Jerry <403727@qq.com>
2019-12-18 15:59:41 +0800

    test  deplopy

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
   if(req.path !== '/' && !req.path.includes('.')){
     res.set({
       'Access-Control-Allow-Credentials': true,
-      'Access-Control-Allow-Origin': req.headers.origin || '*',
+      'Access-Control-Allow-Origin': req.headers.origin,
       'Access-Control-Allow-Headers': 'X-Requested-With,Content-Type',
       'Access-Control-Allow-Methods': 'PUT,POST,GET,DELETE,OPTIONS',
       'Content-Type': 'application/json; charset=utf-8'
@@ -82,7 +82,7 @@ fs.readdirSync(path.join(__dirname, 'module')).reverse().forEach(file => {
   })
 })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 80
 const host = process.env.HOST || ''
 
 app.server = app.listen(port, host, () => {
